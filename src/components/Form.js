0ecfbc3d1942f4895076d0563bb4ef954053acf8
Julia Burundarena <juliaburundarena@MacBook-Pro-de-Julia.local>
2022-03-29 12:34:49 -0300

    autofocus en input implementado

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,15 +19,15 @@ const Form = ({setInputText, todos, setTodos, inputText, setStatus}) => {
 
     return(
         <form>
-          <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+          <input autoFocus value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
           <button onClick={submitTodoHandler} className="todo-button" type="submit">
           <i className="fas fa-plus-square"></i>
           </button>
         <div className="select">
           <select onChange={statusHandler} name="todos" className="filter-todo">
-           <option value="all">All</option>
-           <option value="completed">Completed</option>
-           <option value="uncompleted">Uncompleted</option>
+           <option value="all">Todas</option>
+           <option value="completed">Completadas</option>
+           <option value="uncompleted">Pendientes</option>
           </select>
         </div>
     </form>
